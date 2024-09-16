ed6f88e95deb39ab997203cc922afbfdbf523ea3
axeincode <phpcookerme@gmail.com>
2024-09-16 12:40:38 -0700

    [feat]: added horizontal list

--- lib/main.dart
+++ lib/main.dart
@@ -1,41 +1,53 @@
 import 'package:flutter/material.dart';
 
 void main() {
-    runApp(const MyApp());
+  runApp(MyApp());
 }
 
 
 class MyApp extends StatelessWidget {
-    const MyApp({super.key});
-    
-    @override
+  const MyApp({super.key});
+
+  @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    const title = 'Basic List';
+    const String title = 'Horizontal List';
     
     return MaterialApp(
-        title: title,
-        home: Scaffold(
-            appBar: AppBar(
-                title: const Text(title),
-            ),
-            body: ListView(
-                children: const [
-                    ListTile(
-                        leading: Icon(Icons.map),
-                        title: Text('Map'),
-                    ),
-                    ListTile(
-                        leading: Icon(Icons.photo_album),
-                        title: Text('Album'),
-                    ),
-                    ListTile(
-                        leading: Icon(Icons.phone),
-                        title: Text('Phone'),
-                    )
-                ],
-            ),
+      title: title,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text(title),
         ),
+        body: Container(
+          margin: const EdgeInsets.symmetric(vertical: 20),
+          height: 200,
+          child: ListView(
+            scrollDirection: Axis.horizontal,
+            children: <Widget>[
+              Container(
+                width: 160,
+                color: Colors.red,
+              ),
+              Container(
+                width: 160,
+                color: Colors.blue,
+              ),
+              Container(
+                width: 160,
+                color: Colors.green,
+              ),
+              Container(
+                width: 160,
+                color: Colors.yellow,
+              ),
+              Container(
+                width: 160,
+                color: Colors.orange,
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
